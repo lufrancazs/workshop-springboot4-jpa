ec93055f6a33436518fbb29d0defeb03288c38ec
Lucas Martins <lucasfranca_zs@hotmail.com>
2024-01-19 20:24:45 -0300

    Exception handling - delete

--- src/main/java/com/lucasdefranca/course/services/UserService.java
+++ src/main/java/com/lucasdefranca/course/services/UserService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.lucasdefranca.course.entities.User;
 import com.lucasdefranca.course.repositories.UserRepository;
+import com.lucasdefranca.course.services.exceptions.DatabaseException;
 import com.lucasdefranca.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +32,11 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
